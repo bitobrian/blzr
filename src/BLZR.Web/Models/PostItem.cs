7c0c7f41c9c9196c7fc47d02f027c1387d4b8b2a
Brian Donaldson <nairbsd@gmail.com>
2023-07-20 20:21:10 -0500

    Migrate to .net 8 web app

---
+++ src/BLZR.Web/Models/PostItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BLZR.Web.Models;
+
+public class PostItem
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string LinkUrl { get; set; }
+    public int Likes { get; set; }
+}
---
+++ src/BLZR.Web/wwwroot/js/app.js
@@ -0,0 +1,14 @@
+﻿window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
